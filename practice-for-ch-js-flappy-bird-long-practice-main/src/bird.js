e23d3dba58d04b7a82c77c109ea6f38af2d17ce3
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-27 16:30:58 -0400

    Added bird animate

--- practice-for-ch-js-flappy-bird-long-practice-main/src/bird.js
+++ practice-for-ch-js-flappy-bird-long-practice-main/src/bird.js
@@ -1,12 +1,19 @@
 export default class Bird {
     constructor(dimension) {
         this.velocity = 0;
-        this.dimension = dimension
-        this.x = 320
-        this.y = 160
+        this.dimension = dimension;
+        this.x = 140;
+        this.y = 300;
     }
 
-    
+    drawBird(ctx) {
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(this.x, this.y, 40, 30);
+    }
+
+    animate(ctx){
+        this.drawBird(ctx)
+    }
 
 
 
